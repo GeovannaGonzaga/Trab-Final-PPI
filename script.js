f99699a9c115a8c0fa3ed7588a3123df65f91086
KlariaAndradeMartins <klariaandrade@gmail.com>
2023-10-18 23:11:58 -0300

    Cadastro de produto e mensagens no cadastro de clientes

--- script.js
+++ script.js
@@ -13,4 +13,33 @@ function checkLogin(event) {
       alert('Usuário ou senha incorretos. Tente novamente.');
     }
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const emailInput = document.querySelector("#emailInput");
+    const telefoneInput = document.querySelector("#telefoneInput");
+    const cpfInput = document.querySelector("#cpfInput");
+    const form = document.querySelector("#cadastro-form");
   
+    form.addEventListener("submit", function(event) {
+      let isValid = true;
+  
+      if (emailInput.value.indexOf('@') === -1) {
+        alert("O email precisa conter um '@'.");
+        isValid = false;
+      }
+  
+      if (telefoneInput.value.length < 5) {
+        alert("O número de telefone precisa ter pelo menos 5 dígitos.");
+        isValid = false;
+      }
+  
+      if (cpfInput.value.length < 5) {
+        alert("O CPF precisa ter pelo menos 5 dígitos.");
+        isValid = false;
+      }
+  
+      if (!isValid) {
+        event.preventDefault();
+      }
+    });
+  });
