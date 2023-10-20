a951c32e4d8ce63af6608715bb808d20eab4bfc2
geovanna gonzaga <geovannagonzaga100@gmail.com>
2023-10-19 22:56:44 -0300

    gevanna 8

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function checkLogin(event) {
   const password = document.getElementById('password').value;
 
   // Verifica se o usuário e a senha são 'admin'
-  if (username.toLowerCase() === 'admin' && password.toLowerCase() === 'admin') {
+  if (username.toLowerCase() === 'admin' && password.toLowerCase() === 'admin' ) {
     // Se for 'admin', redireciona para a página admin_control.html
     window.location.href = 'admin_control.html';
   } else if(username.toLowerCase() === 'juliac@gmail.com' && password.toLowerCase() === '12345'){
@@ -16,76 +16,75 @@ function checkLogin(event) {
   }
 }
 // validation
-  document.addEventListener("DOMContentLoaded", function() {
-    const emailInput = document.querySelector("#emailInput");
-    const telefoneInput = document.querySelector("#telefoneInput");
-    const cpfInput = document.querySelector("#cpfInput");
-    const form = document.querySelector("#cadastro-form");
-  
-    form.addEventListener("submit", function(event) {
-      let isValid = true;
-  
-      if (emailInput.value.indexOf('@') === -1) {
-        alert("O email precisa conter um '@'.");
-        isValid = false;
-      }
-  
-      if (telefoneInput.value.length < 5) {
-        alert("O número de telefone precisa ter pelo menos 5 dígitos.");
-        isValid = false;
-      }
-  
-      if (cpfInput.value.length < 5) {
-        alert("O CPF precisa ter pelo menos 5 dígitos.");
-        isValid = false;
-      }
-  
-      if (!isValid) {
-        event.preventDefault();
-      }
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  const emailInput = document.querySelector("#emailInput");
+  const telefoneInput = document.querySelector("#telefoneInput");
+  const cpfInput = document.querySelector("#cpfInput");
+  const form = document.querySelector("#cadastro-form");
+
+  form.addEventListener("submit", function(event) {
+    let isValid = true;
+
+    if (emailInput.value.indexOf('@') === -1) {
+      alert("O email precisa conter um '@'.");
+      isValid = false;
+    }
+
+    if (telefoneInput.value.length < 5) {
+      alert("O número de telefone precisa ter pelo menos 5 dígitos.");
+      isValid = false;
+    }
+
+    if (cpfInput.value.length < 5) {
+      alert("O CPF precisa ter pelo menos 5 dígitos.");
+      isValid = false;
+    }
+
+    if (!isValid) {
+      event.preventDefault();
+    }
   });
+});
 
 // uploadImage
 
 function handleImageUpload() {
-  const imageInput = document.getElementById("imageInput");
-  const imagePreview = document.getElementById("imagePreview");
+const imageInput = document.getElementById("imageInput");
+const imagePreview = document.getElementById("imagePreview");
 
-  imageInput.addEventListener("change", function () {
-    const file = imageInput.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        const img = new Image();
-        img.src = e.target.result;
-        img.style.maxWidth = "100%";
-        imagePreview.innerHTML = ""; // Limpar qualquer imagem anterior
-        imagePreview.appendChild(img);
-      };
-      reader.readAsDataURL(file);
-    }
-  });
+imageInput.addEventListener("change", function () {
+  const file = imageInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const img = new Image();
+      img.src = e.target.result;
+      img.style.maxWidth = "100%";
+      imagePreview.innerHTML = ""; // Limpar qualquer imagem anterior
+      imagePreview.appendChild(img);
+    };
+    reader.readAsDataURL(file);
+  }
+});
 
-  // Impedir o envio do formulário e a mudança de página ao clicar no link "Todos os produtos"
-  const todosOsProdutosLink = document.getElementById("todosOsProdutosLink");
-  todosOsProdutosLink.addEventListener("click", function (event) {
-    event.preventDefault();
-  });
+// Impedir o envio do formulário e a mudança de página ao clicar no link "Todos os produtos"
+const todosOsProdutosLink = document.getElementById("todosOsProdutosLink");
+todosOsProdutosLink.addEventListener("click", function (event) {
+  event.preventDefault();
+});
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  var floatingIcon = document.getElementById("floating-icon");
-  var contactForm = document.getElementById("contact-form");
+var floatingIcon = document.getElementById("floating-icon");
+var contactForm = document.getElementById("contact-form");
 
-  // Quando o ícone é clicado, mostre o formulário
-  floatingIcon.addEventListener("click", function () {
-    contactForm.style.display = "block";
-  });
-
-  // Quando o botão de fechar no formulário é clicado, oculte o formulário
-  document.getElementById("close-form").addEventListener("click", function () {
-    contactForm.style.display = "none";
-  });
+// Quando o ícone é clicado, mostre o formulário
+floatingIcon.addEventListener("click", function () {
+  contactForm.style.display = "block";
 });
 
+// Quando o botão de fechar no formulário é clicado, oculte o formulário
+document.getElementById("close-form").addEventListener("click", function () {
+  contactForm.style.display = "none";
+});
+});
